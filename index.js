a983ae2e1b7f5ee330c4f7af44d364bc025246f4
Pedro Marques <ppmarq1@gmail.com>
2022-04-14 17:51:27 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const workData = [
+    {
+      name: 'Keeping track of hundreds of components 1',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLore",
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1',
+      gitUrl: 'https://github.com/ppmarq1',
+    },
+    {
+      name: 'Keeping track of hundreds of components 2',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Mobile-Version',
+      gitUrl: 'https://github.com/ppmarq1/Mobile-Version',
+    },
+    {
+      name: 'Keeping track of hundreds of components 3',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Mobile-Version',
+      gitUrl: 'https://github.com/ppmarq1/Mobile-Version',
+    },
+    {
+      name: 'Keeping track of hundreds of components 4',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Mobile-Version',
+      gitUrl: 'https://github.com/ppmarq1/Mobile-Version',
+    },
+    {
+      name: 'Keeping track of hundreds of components 5',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Mobile-Version',
+      gitUrl: 'https://github.com/ppmarq1/Mobile-Version',
+    },
+    {
+      name: 'Keeping track of hundreds of components 6',
+      description:
+        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      ImgUrl: 'Snapshoot-Portfolio.svg',
+      tech: ['Ruby on rails', 'css', 'javascript'],
+      demoUrl: 'https://github.com/ppmarq1/Mobile-Version',
+      gitUrl: 'https://github.com/ppmarq1/Mobile-Version',
+    },
+  ];
+  
+  export default workData;
