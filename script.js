c9129256a25d0bb0cfcd9b169dfcb32d6069dff8
Pedro Marques <ppmarq1@gmail.com>
2022-04-16 08:53:42 +0100

    First commit Contact Form Validation

--- script.js
+++ script.js
@@ -104,3 +104,42 @@ ${card.languages}
 
  cardsUl.innerHTML += projectsCards;
  console.log(1234567);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//contact form validation//
+
+const Form = document.querySelector('.form');
+const Email = document.getElementById('email');
+const Name = document.getElementById('name');
+const Comment = document.getElementById('comment');
+const Msg = document.querySelector('.ErrorMessage');
+const reg = /^[a-z0-9_-]+@[a-z0-9]+\.[a-z]+\.?[a-z]+/g;
+
+Form.addEventListener('submit', (e) => {
+  if (!reg.test(Email.value)) {
+    Email.classList.add('invalid-email');
+    Msg.innerHTML = `Error: Email must be lowercase, <br> example: ${Email.value.toLowerCase()}`;
+    e.preventDefault();
+  }
+});
+
+
+
+
+
+
+
+
