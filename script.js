23d2f325536142bc7e2de7a6ea1be7ebf47a5477
Pedro Marques <ppmarq1@gmail.com>
2022-04-16 09:41:54 +0100

    First Commit for Contact Form Validation Project

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ document.querySelectorAll(".nav-link").forEach (n => n.addEventListener("click",
 
 hamburger.addEventListener('click', navMenu);
 document.querySelectorAll(".nav-link"), navMenu.classList.remove('active1');
+
+
+////contact form validation
+
+const Form = document.querySelector('.form');
+const Email = document.getElementById('email');
+const Name = document.getElementById('name');
+const Comment = document.getElementById('comment');
+const Msg = document.querySelector('.ErrorMessage');
+const reg = /^[a-z0-9_-]+@[a-z0-9]+\.[a-z]+\.?[a-z]+/g;
+
